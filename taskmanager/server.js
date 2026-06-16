5578776b261be705f09c84878047a4bc072f2e0f
kelvinkuriam <kelvinkuriam383@gmail.com>
2026-06-16 15:16:06 +0400

    added the app.js which is the express app

--- taskmanager/server.js
+++ taskmanager/server.js
@@ -9,3 +9,11 @@ const server = app.listen(PORT,()=>{
     console.log(`Environment: ${process.env.NODE_ENV}`)
     console.log("Press Ctrl + C to stop the server");
 })
+
+process.on("SIGTERM",()=>{
+    console.log("SIGTERM received, shutting down gracefully...");
+    server.close(()=>{
+        console.log("Server closed. Exiting process.");
+        process.exit(0);
+    })
+})
