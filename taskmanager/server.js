3d7921e8fa9abaa919478ed5d81742c8153cf38f
kelvinkuriam <kelvinkuriam383@gmail.com>
2026-06-16 14:29:51 +0400

    made the server.js

--- taskmanager/server.js
+++ taskmanager/server.js
@@ -1,22 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require("express");
-
-const app = express();
-
+const app = require("./server/app");
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.json({
-        message:"Welcome to the Task Manager API",
-        environment: process.env.NODE_ENV,
-        timestamp: new Date().toISOString()
-    })
-})
-
-app.listen(PORT,()=>{
+const server = app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
-    console.log(`Environment: ${process.env.NODE_ENV}`);
-    console.log(`Press Ctrl+C to stop the server`);
-});
+    console.log(`Environment: ${process.env.NODE_ENV}`)
+    console.log("Press Ctrl + C to stop the server");
+})
